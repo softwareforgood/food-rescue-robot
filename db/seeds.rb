5102a1bf1831003153d08ed007760906a9cedc27
Obinna <softwareforgood@Softwares-MacBook-Pro.local>
2018-04-05 14:38:31 -0500

    changing names of Boulder

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@
 #
 # Examples:
 #
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Emanuel', :city => cities.first)
+#   cities = City.create([{:name: 'Chicago' }, {:name: 'Copenhagen' }])
+#   Mayor.create(:name: 'Emanuel',:city: cities.first)
 
 # Create global transport types
 [
@@ -28,25 +28,30 @@ end
 end
 
 #region.attributes.slice('lat', 'lng', 'name', 'website', 'address', 'notes', 'handbook_url', 'prior_lbs_rescued', 'prior_num_pickups', 'title', 'tagline', 'phone', 'tax_id', 'welcome_email_text', 'splash_html', 'weight_unit', 'time_zone', 'volunteer_coordinator_email', 'post_pickup_emails', 'unschedule_self' )
+#region = Region.find_or_create_by({'name': 'TC ...'})
+#make seeds idempotent
+
+
 region = Region.create({
-  'lat'=> 41.5409097,
-  'lng'=> -72.9990631,
-  'name'=>'Boulder Food Rescue',
-  'website'=>'',
-  'address'=>'Hana Danksy',
-  'notes'=>'',
-  'handbook_url'=>'',
-  'title'=>'Boulder Food Rescue',
-  'tagline'=>'',
-  'phone'=>'208-123-5354',
-  'tax_id'=>'',
-  'welcome_email_text'=>'',
-  'splash_html'=>'',
-  'weight_unit'=>'pound',
-  'time_zone'=>'',
-  'volunteer_coordinator_email'=>'volunteer@gmail.com',
-  'post_pickup_emails'=>false,
-  'unschedule_self'=>false
+  lat: 41.5409097,
+  lng: -72.9990631,
+  name:'TC Food Justice',
+  website:'',
+  website: '',
+  address:'hannah@tcfoodjustice.org',
+  notes:'',
+  handbook_url:'',
+  title:'TC Food Justice',
+  tagline:'',
+  phone:'208-123-5354',
+  tax_id:'',
+  welcome_email_text:'',
+  splash_html:'',
+  weight_unit:'pound',
+  time_zone:'',
+  volunteer_coordinator_email:'volunteer@gmail.com',
+  post_pickup_emails:false,
+  unschedule_self:false
 })
 
 #volunteer.attributes.slice('email', 'name', 'phone', 'preferred_contact', 'has_car', 'admin_notes', 'pickup_prefs', 'is_disabled', 'on_email_list', 'admin', 'transport_type_id', 'cell_carrier_id', 'sms_too', 'pre_reminders_too', 'get_sncs_email', 'waiver_signed', 'waiver_signed_at', 'assigned', 'requested_region_id', 'active')
@@ -54,21 +59,21 @@ region = Region.create({
 
 20.times do |time|
   volunteer = Volunteer.create({
-    'email'=>"volunteer#{time}.bfr@gmail.com",
-     'name'=>"Volunteer#{time}",
-     'phone'=>"760-815-555#{time}",
-     'password' => 'changeme!',
-     'password_confirmation' => 'changeme!',
-     'preferred_contact'=>'Text',
-     'has_car'=>true,
-     'is_disabled'=>false,
-     'on_email_list'=>true,
-     'transport_type_id'=>1,
-     'cell_carrier_id'=>6,
-     'sms_too'=>false,
-     'pre_reminders_too'=>false,
-     'get_sncs_email'=>true,
-     'assigned'=>true
+    email:"volunteer#{time}.TCFJ@gmail.com",
+     name:"Volunteer#{time}",
+     phone:"760-815-555#{time}",
+     password: 'changeme!',
+     password_confirmation: 'changeme!',
+     preferred_contact:'Text',
+     has_car:true,
+     is_disabled:false,
+     on_email_list:true,
+     transport_type_id:1,
+     cell_carrier_id:6,
+     sms_too:false,
+     pre_reminders_too:false,
+     get_sncs_email:true,
+     assigned:true
   })
 end
 
@@ -77,9 +82,9 @@ Availability.destroy_all
 for vol in Volunteer.all
   6.times do |time|
     availability = Availability.create({
-      'volunteer_id'=>"#{time+1})",
-       'day'=>"#{time+1})",
-       'time'=>"{1,2,3}.sample",
+      volunteer_id:"#{time+1})",
+       day:"#{time+1})",
+       time:"{1,2,3}.sample",
     })
   end
 end
@@ -98,21 +103,21 @@ assignment.region_id = region.id
 assignment.save!
 
 super_admin = Volunteer.create({
-  'email'=>'superadmin.bfr@gmail.com',
-   'name'=>'Super Admin',
-   'password' => 'changeme!',
-   'password_confirmation' => 'changeme!',
-   'phone'=>'760-888-5555',
-   'preferred_contact'=>'Text',
-   'has_car'=>true,
-   'is_disabled'=>false,
-   'on_email_list'=>true,
-   'transport_type_id'=>1,
-   'cell_carrier_id'=>6,
-   'sms_too'=>false,
-   'pre_reminders_too'=>false,
-   'get_sncs_email'=>true,
-   'assigned'=>true
+  email:'superadmin.TCFJ@gmail.com',
+   name:'Super Admin',
+   password: 'changeme!',
+   password_confirmation: 'changeme!',
+   phone:'760-888-5555',
+   preferred_contact:'Text',
+   has_car:true,
+   is_disabled:false,
+   on_email_list:true,
+   transport_type_id:1,
+   cell_carrier_id:6,
+   sms_too:false,
+   pre_reminders_too:false,
+   get_sncs_email:true,
+   assigned:true
 })
 
 super_admin.admin = true
@@ -123,21 +128,21 @@ super_admin.save
 
 
 region_admin = Volunteer.create({
-  'email'=>'regionadmin.bfr@gmail.com',
-   'name'=>"Region Admin #{region.name}",
-   'phone'=>'760-888-5555',
-   'password' => 'changeme!',
-   'password_confirmation' => 'changeme!',
-   'preferred_contact'=>'Text',
-   'has_car'=>true,
-   'is_disabled'=>false,
-   'on_email_list'=>true,
-   'transport_type_id'=>1,
-   'cell_carrier_id'=>6,
-   'sms_too'=>false,
-   'pre_reminders_too'=>false,
-   'get_sncs_email'=>true,
-   'assigned'=>true
+  email:'regionadmin.TCFJ@gmail.com',
+   name:"Region Admin #{region.name}",
+   phone:'760-888-5555',
+   password: 'changeme!',
+   password_confirmation: 'changeme!',
+   preferred_contact:'Text',
+   has_car:true,
+   is_disabled:false,
+   on_email_list:true,
+   transport_type_id:1,
+   cell_carrier_id:6,
+   sms_too:false,
+   pre_reminders_too:false,
+   get_sncs_email:true,
+   assigned:true
 })
 
 region_admin.admin = false
@@ -156,55 +161,55 @@ admin_assignment.save!
 
 #Create donor location
 donor = Location.create({
-     'location_type' => 1,
-     'name' => "Test Donor",
-     'active' => true,
-     'region_id' => region.id,
-     'address' => "123 Side St."
+     location_type: 1,
+     name: "Test Donor",
+     active: true,
+     region_id: region.id,
+     address: "123 Side St."
 })
 #Create recipient location
 
 recipient = Location.create({
-     'location_type' => 0,
-     'name' => "Test Recipient",
-     'active' => true,
-     'region_id' => region.id,
-     'address' => "123 Main St."
+     location_type: 0,
+     name: "Test Recipient",
+     active: true,
+     region_id: region.id,
+     address: "123 Main St."
 
 })
 
 #Create schedule chain
 
 schedule_chain = ScheduleChain.create({
-     'detailed_start_time' => "2000-01-01 08:00:00",
-     'detailed_stop_time' => "2000-01-01 11:00:00",
-     'detailed_date' => "2018-02-28",
-     'transport_type_id' => 2,
-     'region_id'=> region.id,
-     'frequency'=> 'weekly',
-     'day_of_week'=> 1
+     detailed_start_time: "2000-01-01 08:00:00",
+     detailed_stop_time: "2000-01-01 11:00:00",
+     detailed_date: "2018-02-28",
+     transport_type_id: 2,
+     region_id: region.id,
+     frequency: 'weekly',
+     day_of_week: 1
 })
 
 #Assign donor location to schedule
 Schedule.create({
-    'schedule_chain_id' => schedule_chain.id,
-    'location_id' => donor.id,
-    'position' => 1
+    schedule_chain_id: schedule_chain.id,
+    location_id: donor.id,
+    position: 1
 })
 
 #Assign recipient location to schedule
 Schedule.create({
-    'schedule_chain_id' => schedule_chain.id,
-    'location_id' => recipient.id,
-    'position' => 2
+    schedule_chain_id: schedule_chain.id,
+    location_id: recipient.id,
+    position: 2
 })
 
 #Assign schedule to volunteer
 
 ScheduleVolunteer.create({
-    'volunteer_id' => volunteer.id,
-    'schedule_chain_id' => schedule_chain.id,
-    'active' => true
+    volunteer_id: volunteer.id,
+    schedule_chain_id: schedule_chain.id,
+    active: true
 })
 
 
