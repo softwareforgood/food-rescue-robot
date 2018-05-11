740002c10f8eca2b2788f3d43c6fae1a22c361c8
Eddie <glen0071@gmail.com>
2018-05-11 07:04:40 -0700

    WIP prevent duplicate availabilities created

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -1,4 +1,10 @@
 class Availability < ActiveRecord::Base
   belongs_to :volunteer
   attr_accessible :day, :time, :volunteer_id
+
+  before_save :availability_already_exists
+
+  def availability_already_exists
+    Availability.find(day: day, time: day, volunteer_id: volunteer_id).empty? ? true : false
+  end
 end
